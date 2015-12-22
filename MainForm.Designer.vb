7e518e207e04f7a86721a0d11ad7595415618a39
unknown <lopeze@FR-L27131.groupinfra.com>
2015-12-22 17:21:28 +0100

    Version béta de l'interface d'exécution des scripts SQL

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -33,168 +33,231 @@ Partial Class MainForm
 	''' not be able to load this method if it was changed manually.
 	''' </summary>
 	Private Sub InitializeComponent()
-		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
-		Me.pictTitle = New System.Windows.Forms.PictureBox()
-		Me.lblServer = New System.Windows.Forms.Label()
-		Me.lblDatabase = New System.Windows.Forms.Label()
-		Me.lblUser = New System.Windows.Forms.Label()
-		Me.lblPasswd = New System.Windows.Forms.Label()
-		Me.cbxServer = New System.Windows.Forms.ComboBox()
-		Me.txtUser = New System.Windows.Forms.TextBox()
-		Me.txtPass = New System.Windows.Forms.TextBox()
-		Me.cbxDatabase = New System.Windows.Forms.ComboBox()
-		Me.lblScript = New System.Windows.Forms.Label()
-		Me.txtScript = New System.Windows.Forms.TextBox()
-		Me.btnScriptPath = New System.Windows.Forms.Button()
-		Me.btnQuit = New System.Windows.Forms.Button()
+		Me.tabCtl = New System.Windows.Forms.TabControl()
+		Me.tabPgSQL = New System.Windows.Forms.TabPage()
+		Me.btnConnect = New System.Windows.Forms.Button()
 		Me.btnExec = New System.Windows.Forms.Button()
-		CType(Me.pictTitle,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.btnScriptPath = New System.Windows.Forms.Button()
+		Me.txtScript = New System.Windows.Forms.TextBox()
+		Me.lblScript = New System.Windows.Forms.Label()
+		Me.cbxDatabase = New System.Windows.Forms.ComboBox()
+		Me.txtPass = New System.Windows.Forms.TextBox()
+		Me.txtUser = New System.Windows.Forms.TextBox()
+		Me.lblPasswd = New System.Windows.Forms.Label()
+		Me.lblUser = New System.Windows.Forms.Label()
+		Me.lblDatabase = New System.Windows.Forms.Label()
+		Me.txtLogSQL = New System.Windows.Forms.TextBox()
+		Me.cbxServer = New System.Windows.Forms.ComboBox()
+		Me.lblServer = New System.Windows.Forms.Label()
+		Me.tabPgWeb = New System.Windows.Forms.TabPage()
+		Me.btnQuit = New System.Windows.Forms.Button()
+		Me.tabCtl.SuspendLayout
+		Me.tabPgSQL.SuspendLayout
 		Me.SuspendLayout
 		'
-		'pictTitle
+		'tabCtl
 		'
-		Me.pictTitle.ErrorImage = Nothing
-		Me.pictTitle.Image = CType(resources.GetObject("pictTitle.Image"),System.Drawing.Image)
-		Me.pictTitle.InitialImage = Nothing
-		Me.pictTitle.Location = New System.Drawing.Point(1, 2)
-		Me.pictTitle.Name = "pictTitle"
-		Me.pictTitle.Size = New System.Drawing.Size(477, 84)
-		Me.pictTitle.TabIndex = 0
-		Me.pictTitle.TabStop = false
+		Me.tabCtl.Controls.Add(Me.tabPgSQL)
+		Me.tabCtl.Controls.Add(Me.tabPgWeb)
+		Me.tabCtl.Location = New System.Drawing.Point(12, 12)
+		Me.tabCtl.Name = "tabCtl"
+		Me.tabCtl.SelectedIndex = 0
+		Me.tabCtl.Size = New System.Drawing.Size(447, 416)
+		Me.tabCtl.TabIndex = 0
 		'
-		'lblServer
+		'tabPgSQL
 		'
-		Me.lblServer.Location = New System.Drawing.Point(55, 137)
-		Me.lblServer.Name = "lblServer"
-		Me.lblServer.Size = New System.Drawing.Size(63, 23)
-		Me.lblServer.TabIndex = 1
-		Me.lblServer.Text = "Serveur : "
+		Me.tabPgSQL.Controls.Add(Me.btnConnect)
+		Me.tabPgSQL.Controls.Add(Me.btnExec)
+		Me.tabPgSQL.Controls.Add(Me.btnScriptPath)
+		Me.tabPgSQL.Controls.Add(Me.txtScript)
+		Me.tabPgSQL.Controls.Add(Me.lblScript)
+		Me.tabPgSQL.Controls.Add(Me.cbxDatabase)
+		Me.tabPgSQL.Controls.Add(Me.txtPass)
+		Me.tabPgSQL.Controls.Add(Me.txtUser)
+		Me.tabPgSQL.Controls.Add(Me.lblPasswd)
+		Me.tabPgSQL.Controls.Add(Me.lblUser)
+		Me.tabPgSQL.Controls.Add(Me.lblDatabase)
+		Me.tabPgSQL.Controls.Add(Me.txtLogSQL)
+		Me.tabPgSQL.Controls.Add(Me.cbxServer)
+		Me.tabPgSQL.Controls.Add(Me.lblServer)
+		Me.tabPgSQL.Location = New System.Drawing.Point(4, 22)
+		Me.tabPgSQL.Name = "tabPgSQL"
+		Me.tabPgSQL.Padding = New System.Windows.Forms.Padding(3)
+		Me.tabPgSQL.Size = New System.Drawing.Size(439, 390)
+		Me.tabPgSQL.TabIndex = 0
+		Me.tabPgSQL.Text = "Scripts SQL"
+		Me.tabPgSQL.UseVisualStyleBackColor = true
 		'
-		'lblDatabase
+		'btnConnect
 		'
-		Me.lblDatabase.Location = New System.Drawing.Point(55, 255)
-		Me.lblDatabase.Name = "lblDatabase"
-		Me.lblDatabase.Size = New System.Drawing.Size(100, 23)
-		Me.lblDatabase.TabIndex = 2
-		Me.lblDatabase.Text = "Base de données : "
+		Me.btnConnect.Enabled = false
+		Me.btnConnect.Location = New System.Drawing.Point(321, 46)
+		Me.btnConnect.Name = "btnConnect"
+		Me.btnConnect.Size = New System.Drawing.Size(84, 26)
+		Me.btnConnect.TabIndex = 48
+		Me.btnConnect.Text = "Connecter"
+		Me.btnConnect.UseVisualStyleBackColor = true
+		AddHandler Me.btnConnect.Click, AddressOf Me.BtnConnectClick
 		'
-		'lblUser
+		'btnExec
 		'
-		Me.lblUser.Location = New System.Drawing.Point(55, 172)
-		Me.lblUser.Name = "lblUser"
-		Me.lblUser.Size = New System.Drawing.Size(100, 23)
-		Me.lblUser.TabIndex = 3
-		Me.lblUser.Text = "Utilisateur : "
+		Me.btnExec.Enabled = false
+		Me.btnExec.Location = New System.Drawing.Point(321, 226)
+		Me.btnExec.Name = "btnExec"
+		Me.btnExec.Size = New System.Drawing.Size(84, 28)
+		Me.btnExec.TabIndex = 47
+		Me.btnExec.Text = "Exécuter"
+		Me.btnExec.UseVisualStyleBackColor = true
+		AddHandler Me.btnExec.Click, AddressOf Me.BtnExecClick
+		'
+		'btnScriptPath
+		'
+		Me.btnScriptPath.Enabled = false
+		Me.btnScriptPath.Location = New System.Drawing.Point(93, 200)
+		Me.btnScriptPath.Name = "btnScriptPath"
+		Me.btnScriptPath.Size = New System.Drawing.Size(34, 23)
+		Me.btnScriptPath.TabIndex = 46
+		Me.btnScriptPath.Text = "..."
+		Me.btnScriptPath.UseVisualStyleBackColor = true
+		AddHandler Me.btnScriptPath.Click, AddressOf Me.BtnScriptPathClick
+		'
+		'txtScript
+		'
+		Me.txtScript.Enabled = false
+		Me.txtScript.Location = New System.Drawing.Point(133, 200)
+		Me.txtScript.Name = "txtScript"
+		Me.txtScript.Size = New System.Drawing.Size(272, 20)
+		Me.txtScript.TabIndex = 45
+		'AddHandler Me.txtScript.TextChanged, AddressOf Me.TxtScript.TextChanged
+		'
+		'lblScript
+		'
+		Me.lblScript.Location = New System.Drawing.Point(27, 205)
+		Me.lblScript.Name = "lblScript"
+		Me.lblScript.Size = New System.Drawing.Size(100, 23)
+		Me.lblScript.TabIndex = 44
+		Me.lblScript.Text = "Script SQL : "
+		'
+		'cbxDatabase
+		'
+		Me.cbxDatabase.Enabled = false
+		Me.cbxDatabase.FormattingEnabled = true
+		Me.cbxDatabase.Location = New System.Drawing.Point(133, 103)
+		Me.cbxDatabase.Name = "cbxDatabase"
+		Me.cbxDatabase.Size = New System.Drawing.Size(272, 21)
+		Me.cbxDatabase.TabIndex = 43
+		AddHandler Me.cbxDatabase.SelectedIndexChanged, AddressOf Me.CbxDatabaseSelectedIndexChanged
+		'
+		'txtPass
+		'
+		Me.txtPass.Enabled = false
+		Me.txtPass.Location = New System.Drawing.Point(133, 166)
+		Me.txtPass.Name = "txtPass"
+		Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(176)
+		Me.txtPass.Size = New System.Drawing.Size(272, 20)
+		Me.txtPass.TabIndex = 42
+		'
+		'txtUser
+		'
+		Me.txtUser.Enabled = false
+		Me.txtUser.Location = New System.Drawing.Point(133, 134)
+		Me.txtUser.Name = "txtUser"
+		Me.txtUser.Size = New System.Drawing.Size(272, 20)
+		Me.txtUser.TabIndex = 41
 		'
 		'lblPasswd
 		'
-		Me.lblPasswd.Location = New System.Drawing.Point(55, 204)
+		Me.lblPasswd.Location = New System.Drawing.Point(27, 166)
 		Me.lblPasswd.Name = "lblPasswd"
 		Me.lblPasswd.Size = New System.Drawing.Size(100, 23)
-		Me.lblPasswd.TabIndex = 4
+		Me.lblPasswd.TabIndex = 40
 		Me.lblPasswd.Text = "Mot de passe : "
+		'
+		'lblUser
+		'
+		Me.lblUser.Location = New System.Drawing.Point(27, 137)
+		Me.lblUser.Name = "lblUser"
+		Me.lblUser.Size = New System.Drawing.Size(100, 26)
+		Me.lblUser.TabIndex = 39
+		Me.lblUser.Text = "Utilisateur : "
+		'
+		'lblDatabase
+		'
+		Me.lblDatabase.Location = New System.Drawing.Point(27, 106)
+		Me.lblDatabase.Name = "lblDatabase"
+		Me.lblDatabase.Size = New System.Drawing.Size(100, 23)
+		Me.lblDatabase.TabIndex = 38
+		Me.lblDatabase.Text = "Base de données : "
+		'
+		'txtLogSQL
+		'
+		Me.txtLogSQL.AcceptsReturn = true
+		Me.txtLogSQL.Location = New System.Drawing.Point(27, 279)
+		Me.txtLogSQL.Multiline = true
+		Me.txtLogSQL.Name = "txtLogSQL"
+		Me.txtLogSQL.ReadOnly = true
+		Me.txtLogSQL.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+		Me.txtLogSQL.Size = New System.Drawing.Size(378, 93)
+		Me.txtLogSQL.TabIndex = 28
 		'
 		'cbxServer
 		'
 		Me.cbxServer.FormattingEnabled = true
-		Me.cbxServer.Location = New System.Drawing.Point(161, 137)
+		Me.cbxServer.Location = New System.Drawing.Point(133, 19)
 		Me.cbxServer.Name = "cbxServer"
-		Me.cbxServer.Size = New System.Drawing.Size(256, 21)
-		Me.cbxServer.TabIndex = 5
+		Me.cbxServer.Size = New System.Drawing.Size(272, 21)
+		Me.cbxServer.TabIndex = 19
+		AddHandler Me.cbxServer.SelectedIndexChanged, AddressOf Me.CbxServerSelectedIndexChanged
 		'
-		'txtUser
+		'lblServer
 		'
-		Me.txtUser.Location = New System.Drawing.Point(161, 172)
-		Me.txtUser.Name = "txtUser"
-		Me.txtUser.Size = New System.Drawing.Size(256, 20)
-		Me.txtUser.TabIndex = 6
+		Me.lblServer.Location = New System.Drawing.Point(27, 17)
+		Me.lblServer.Name = "lblServer"
+		Me.lblServer.Size = New System.Drawing.Size(63, 23)
+		Me.lblServer.TabIndex = 15
+		Me.lblServer.Text = "Serveur : "
 		'
-		'txtPass
+		'tabPgWeb
 		'
-		Me.txtPass.Location = New System.Drawing.Point(161, 204)
-		Me.txtPass.Name = "txtPass"
-		Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(176)
-		Me.txtPass.Size = New System.Drawing.Size(256, 20)
-		Me.txtPass.TabIndex = 7
-		'
-		'cbxDatabase
-		'
-		Me.cbxDatabase.FormattingEnabled = true
-		Me.cbxDatabase.Location = New System.Drawing.Point(161, 252)
-		Me.cbxDatabase.Name = "cbxDatabase"
-		Me.cbxDatabase.Size = New System.Drawing.Size(256, 21)
-		Me.cbxDatabase.TabIndex = 8
-		'
-		'lblScript
-		'
-		Me.lblScript.Location = New System.Drawing.Point(55, 288)
-		Me.lblScript.Name = "lblScript"
-		Me.lblScript.Size = New System.Drawing.Size(100, 23)
-		Me.lblScript.TabIndex = 9
-		Me.lblScript.Text = "Script SQL : "
-		'
-		'txtScript
-		'
-		Me.txtScript.Location = New System.Drawing.Point(161, 285)
-		Me.txtScript.Name = "txtScript"
-		Me.txtScript.PasswordChar = Global.Microsoft.VisualBasic.ChrW(176)
-		Me.txtScript.Size = New System.Drawing.Size(256, 20)
-		Me.txtScript.TabIndex = 10
-		'
-		'btnScriptPath
-		'
-		Me.btnScriptPath.Location = New System.Drawing.Point(424, 285)
-		Me.btnScriptPath.Name = "btnScriptPath"
-		Me.btnScriptPath.Size = New System.Drawing.Size(34, 23)
-		Me.btnScriptPath.TabIndex = 11
-		Me.btnScriptPath.Text = "..."
-		Me.btnScriptPath.UseVisualStyleBackColor = true
+		Me.tabPgWeb.Location = New System.Drawing.Point(4, 22)
+		Me.tabPgWeb.Name = "tabPgWeb"
+		Me.tabPgWeb.Padding = New System.Windows.Forms.Padding(3)
+		Me.tabPgWeb.Size = New System.Drawing.Size(439, 390)
+		Me.tabPgWeb.TabIndex = 1
+		Me.tabPgWeb.Text = "Web"
+		Me.tabPgWeb.UseVisualStyleBackColor = true
 		'
 		'btnQuit
 		'
-		Me.btnQuit.Location = New System.Drawing.Point(374, 346)
+		Me.btnQuit.Location = New System.Drawing.Point(371, 434)
 		Me.btnQuit.Name = "btnQuit"
 		Me.btnQuit.Size = New System.Drawing.Size(84, 33)
-		Me.btnQuit.TabIndex = 12
+		Me.btnQuit.TabIndex = 26
 		Me.btnQuit.Text = "Quitter"
 		Me.btnQuit.UseVisualStyleBackColor = true
 		AddHandler Me.btnQuit.Click, AddressOf Me.BtnQuitClick
-		'
-		'btnExec
-		'
-		Me.btnExec.Location = New System.Drawing.Point(266, 346)
-		Me.btnExec.Name = "btnExec"
-		Me.btnExec.Size = New System.Drawing.Size(84, 33)
-		Me.btnExec.TabIndex = 13
-		Me.btnExec.Text = "Exécuter"
-		Me.btnExec.UseVisualStyleBackColor = true
 		'
 		'MainForm
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(478, 402)
-		Me.Controls.Add(Me.btnExec)
+		Me.ClientSize = New System.Drawing.Size(468, 473)
+		Me.Controls.Add(Me.tabCtl)
 		Me.Controls.Add(Me.btnQuit)
-		Me.Controls.Add(Me.btnScriptPath)
-		Me.Controls.Add(Me.txtScript)
-		Me.Controls.Add(Me.lblScript)
-		Me.Controls.Add(Me.cbxDatabase)
-		Me.Controls.Add(Me.txtPass)
-		Me.Controls.Add(Me.txtUser)
-		Me.Controls.Add(Me.cbxServer)
-		Me.Controls.Add(Me.lblPasswd)
-		Me.Controls.Add(Me.lblUser)
-		Me.Controls.Add(Me.lblDatabase)
-		Me.Controls.Add(Me.lblServer)
-		Me.Controls.Add(Me.pictTitle)
 		Me.Name = "MainForm"
 		Me.Text = "MaEVA - Déploiement SQL"
 		AddHandler Load, AddressOf Me.MainFormLoad
-		CType(Me.pictTitle,System.ComponentModel.ISupportInitialize).EndInit
+		Me.tabCtl.ResumeLayout(false)
+		Me.tabPgSQL.ResumeLayout(false)
+		Me.tabPgSQL.PerformLayout
 		Me.ResumeLayout(false)
-		Me.PerformLayout
 	End Sub
+	Private btnConnect As System.Windows.Forms.Button
+	Private txtLogSQL As System.Windows.Forms.TextBox
+	Private tabPgWeb As System.Windows.Forms.TabPage
+	Private tabPgSQL As System.Windows.Forms.TabPage
+	Private tabCtl As System.Windows.Forms.TabControl
 	Private btnExec As System.Windows.Forms.Button
 	Private btnQuit As System.Windows.Forms.Button
 	Private btnScriptPath As System.Windows.Forms.Button
@@ -208,5 +271,4 @@ Partial Class MainForm
 	Private lblUser As System.Windows.Forms.Label
 	Private lblDatabase As System.Windows.Forms.Label
 	Private lblServer As System.Windows.Forms.Label
-	Private pictTitle As System.Windows.Forms.PictureBox
 End Class
--- MainForm.vb
+++ MainForm.vb
@@ -6,41 +6,185 @@
 ' 
 ' To change this template use Tools | Options | Coding | Edit Standard Headers.
 '
-
-Imports System.Data.DataTable
 Imports System.data
+Imports System.Data.DataTable
 Imports System.Data.Sql
+Imports System.Data.SqlClient
 
 Public Partial Class MainForm
 	Public Sub New()
-		' The Me.InitializeComponent call is required for Windows Forms designer support.
-		Me.InitializeComponent()
 		
-		'
-		' TODO : Add constructor code after InitializeComponents
-		'
+		Me.InitializeComponent()
+
 	End Sub
 	
 	Sub MainFormLoad(sender As Object, e As EventArgs)
-		alimentCbxServer()
+		getServerList()
 	End Sub
 	
 	Sub BtnQuitClick(sender As Object, e As EventArgs)
-		Me.Close()		
+		Me.Close()	
 	End Sub
 	
 	
 	'---------------------------------------------------------------------
-	'
-	' Alimentation de la combobox des instances SQL Server disponibles
+	' EVENT : Lorsque le serveur SQL est choisi
+	'---------------------------------------------------------------------	
+	Sub CbxServerSelectedIndexChanged(sender As Object, e As EventArgs)
+		txtUser.Enabled = True
+		txtPass.Enabled = True
+		btnConnect.Enabled = True
+		cbxDatabase.Items.Clear()
+		'getDatabaseList()
+	End Sub
+	
+
 	'---------------------------------------------------------------------
-	Sub alimentCbxServer ()
+	' EVENT : Connexion au serveur SQL sélectionné 
+	'---------------------------------------------------------------------	
+	Sub BtnConnectClick(sender As Object, e As EventArgs)
+		txtLogSQL.Text = "Connexion en cours ..."
+		getDatabaseList()		
+	End Sub
 		
+		
+	'------------------------------------------------------
+	' EVENT : choix de la base de données
+	'------------------------------------------------------	
+	Sub CbxDatabaseSelectedIndexChanged(sender As Object, e As EventArgs)
+		txtScript.Enabled = True
+		btnScriptPath.Enabled = true
+	End Sub
+	
+	
+	'------------------------------------------------------
+	' EVENT : demande exécution du fichier de script sélectionné
+	'------------------------------------------------------
+	Sub BtnExecClick(sender As Object, e As EventArgs)
+		execSqlScript()
+	End Sub
+	
+	'------------------------------------------------------
+	' EVENT : séléction du fichier de script à exécuter
+	'------------------------------------------------------
+	Sub BtnScriptPathClick(sender As Object, e As EventArgs)
+		getSqlFile()
+	End Sub
+	
+	
+	'---------------------------------------------------------------------
+	' PROC : Création de la liste des instances SQL Server disponibles
+	'---------------------------------------------------------------------
+	Sub getServerList ()
+				
 		Dim dt As Data.DataTable
-		dt = SqlDataSourceEnumerator.Instance.GetDataSources		
-		For Each dr As Data.DataRow  In dt.Rows
-		     cbxServer.Items.Add(String.Concat(dr("ServerName"), "\", dr("InstanceName")))
-		Next
-	End sub
+		Dim dr As DataRow
+		Dim msg As String
+		
+		msg = ""
+		dt = SqlDataSourceEnumerator.Instance.GetDataSources()
+		
+		Try	
+			For Each dr In dt.Rows
+		    	cbxServer.Items.Add(dr.Item(0).ToString & "\" & dr.Item(1).ToString)
+			Next
+			msg = "La liste des SQL servers est à jour."
+			
+		Catch e As Exception
+			msg = "Aucun SQL server n'a pas été trouvé." & vbCrLf
+			msg = msg & e.Message
+			
+		Finally
+			txtLogSQL.Clear()
+			txtLogSQL.Text = msg
+			dr = Nothing
+			dt =  Nothing
+		End Try
+		
+	End Sub		
+	
+	
+	'---------------------------------------------------------------------
+	' PROC : Connexion au serveur sélectionné et 
+	' 		 récupération de la liste des bases de données
+	'---------------------------------------------------------------------	
+	Sub getDatabaseList ()
+		
+		Dim sqlServerName As String
+		Dim sqlCnx As New SqlClient.SqlConnection
+		Dim sqlCmd As New SqlCommand
+		Dim cnxString As String
+		Dim reader As SqlDataReader
+		Dim msg As String
+		
+		sqlServerName = cbxServer.Text 
+		msg = ""
+		txtLogSQL.Clear()
+		cnxString = "integrated security=SSPI;data source=" & sqlServerName & ";" & "persist security info=False;initial catalog="
+		
+		Try
+			sqlCmd.CommandText = "SELECT name, database_id, create_date FROM sys.databases ;"
+			sqlCmd.CommandType = CommandType.Text
+			sqlCmd.Connection = sqlCnx
+			sqlCnx.ConnectionString = cnxString
+			sqlCnx.Open()
+		
+			reader = sqlCmd.ExecuteReader()
+			
+			'Nettoyage de la liste avant remplissage
+			cbxDatabase.Items.Clear()
+			
+			'remplissage de la liste
+			While reader.Read()
+				cbxDatabase.Items.Add(reader.GetString(0))
+			End While
+		
+		sqlCnx.Close()
+		cbxDatabase.Enabled = true
+
+		Catch e As Exception
+			msg = e.Message
+		Finally
+			txtLogSQL.Text = msg
+		End Try
+	End Sub	
+	
+	
+	
+	'--------------------------------------------------------
+	' PROC : Récupération du script sql à exécuter
+	'---------------------------------------------------------
+	Sub getSqlFile()
+				Dim sqlFileDialog As New OpenFileDialog()
+		try
+			sqlFileDialog.Filter = "Scripts SQL (*.sql)|*.sql"
+			sqlFileDialog.Multiselect = False
+			sqlFileDialog.ShowDialog()
+			txtScript.text = sqlFileDialog.FileName
+			Catch  e As Exception
+				txtLogSQL.text = e.message
+			Finally
+		End Try
+	End Sub
+	
+	
+	'------------------------------------------------------------
+	' Exécution du script sql sélectionné
+	'------------------------------------------------------------
+	' TODO 
+	Sub execSqlScript()
+		'If (UserClickedOK = True) Then
+        'Open the selected file to read.
+       ' Dim fileStream As System.IO.Stream = sqlFileDialog.File.OpenRead
+'
+        'Using reader As New System.IO.StreamReader(fileStream)
+         '        sqlTxt = sqlTxt & reader.ReadLine
+        'End Using
+        ' fileStream.Close()
+        txtLogSQL.Text = "Exécution du script sélectionné"
+	End Sub
+	
+	
+
 	
 End Class
